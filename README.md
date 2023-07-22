9148facf8ee5ca10c6459d115be314451d01ef30
StingLee <mocca_lee@outlook.com>
2023-07-22 14:45:32 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # CommercialStockXAPI
+
+CommercialStockXAPI is a project that provides remote HTTP access to the StockX API. It supports all interfaces of this platforms and serves as a global sneaker seller's tool, assisting in automating processes such as selling sneakers. The interfaces are actively maintained to ensure long-term availability.
+
+For detailed information about the interfaces, please refer to the following guides:
+- [StockX API documentation(Online testing interface)](http://stockxapi.spiderx.cc:61030/docs)
+
+Python sample code can be found in the `python` directory.
+
+## Supported Platform
+<img src="/res/stockx.jpg" width="100" height="100" alt="StockX"/><br/>
+
+If you also need Goat/Alias API, please refer to [CommercialGoatAPI](https://github.com/Sting-Lee/CommercialGoatAPI/)
+
+## Supported Interfaces
+
+CommercialStockXAPI supports the following interfaces for StockX:
+
+  - Init DeviceID
+  - Search for products
+  - Get historical sales records of specified products
+  - Get product details of specified products
+  - Get product size and price information
+  - Get User self information
+  - Get User seller information
+  - Update access_token
+  - Query the listing. Return the product list.
+  - Query the sold. Return the product list.
+  - Query the history sold. Return the product list.
+  - Query the order detail. Return the order detail.
+  - Enable vacation mode
+  - Create shipment
+  - Cancel product listing
+  - Modify/Update product price (individual/batch)
+  - List products for sale (individual/batch)
+
+  - Other interfaces:
+    - Support additional interfaces as per your requirements
+
+## How to Access
+
+For access and inquiries, please reach out to:
+
+- Discord: sting_lee
+- E-Mail: mocca_lee@outlook.com
+
+Contact us to request a trial period (one week). If you have additional requirements or need further assistance, feel free to discuss them with us.
+
+## Quotation
+
+The fee is based on the total monthly call volume and is paid on a monthly basis.
+
+| MonthlyCalls         | Cost（USD）                             | unit-price（USD/Req） |
+| -------------------- | --------------------------------------- | --------------------- |
+| <300,000             | $400                                    | 0.0013/req            |
+| 300,000~2,000,000    | $800                                    | 0.0004/req            |
+| 2,000,000~10,000,000 | $1600                                   | 0.00016/req           |
+| >10,000,000          | Pending(Exclusive deployment resources) | <0.00016/req          |
---
+++ python/stockx_api_sample.py
@@ -0,0 +1,418 @@
+
+import requests
+
+base_url = "http://stockxapi.spiderx.cc:61030"
+# Token needs to be applied for from the author and can be tested this week. Online testing can be seen at http://stockxapi.spiderx.cc:61030/docs
+# Contact the author Email: mocca_lee@outlook.com Discord: sting_lee
+TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+
+def ping():
+    """
+    @description: 测试接口是否可用 Test whether the interface is available
+    @return:
+    """
+    url = f"{base_url}/ping"
+    response = requests.get(url)
+    return response.json()
+
+
+def init_auth():
+    """
+    @description: 初始化auth，初始化一个设备ID，后面所有接口都要用到。该接口较慢。
+    Initialize auth, initialize a device ID, and all subsequent interfaces will be used. This interface is slower.
+    @return:
+    """
+    url = f"{base_url}/api/stockx/init_auth"
+    params = {
+        "token": TOKEN
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def search_product(auth, keyword, page):
+    """
+    @description: 搜索商品 Search for goods
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param keyword: 搜索关键词 Search keywords
+    @param page: 页码 Page number
+    @return:
+    """
+    url = f"{base_url}/api/stockx/search_product"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "keyword": keyword,
+        "page": page
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def product_activity(auth, product_uuid):
+    """
+    @description: 商品活动信息(历史销售记录) Product activity information(historical sales records)
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param product_uuid: 商品ID Product ID
+    @return:
+    """
+    url = f"{base_url}/api/stockx/product_activity"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "product_uuid": product_uuid
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def product_detail(auth, product_uuid):
+    """
+    @description: 商品详情 Product details
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param product_uuid: 商品ID Product ID
+    @return:
+    """
+    url = f"{base_url}/api/stockx/product_detail"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "product_uuid": product_uuid
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def product_size_price(auth, product_id):
+    """
+    @description: 商品尺码价格 Product size price
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param product_id: 商品ID(精确到尺码的ID) Product ID(ID to size)
+    @return:
+    """
+    url = f"{base_url}/api/stockx/product_size_price"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "product_id": product_id,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def self_info(auth, access_token):
+    """
+    @description: 用户信息 User information
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @return:
+    """
+    url = f"{base_url}/api/stockx/self_info"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def seller_info(auth, access_token):
+    """
+    @description: 卖家信息 Seller information
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @return:
+    """
+    url = f"{base_url}/api/stockx/seller_info"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def update_access_token(auth, refresh_token, client_id, auth0_client):
+    """
+    @description: 更新access_token Update access_token
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param refresh_token: 用户登录后的refresh_token参数 refresh_token parameter after user login
+    @param client_id:
+    @param auth0_client:
+    @return:
+    """
+    url = f"{base_url}/api/stockx/update_access_token"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "auth0_client": auth0_client,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def get_selling_products(auth, access_token, cursor="", query=""):
+    """
+    @description: 获取正在出售中的商品 Get products on sale
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param cursor:
+    @param query: 查询关键词，可为空 Query keywords, can be empty
+    @return:
+    """
+    url = f"{base_url}/api/stockx/get_selling_products"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "cursor": cursor,
+        "query": query,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def get_sold_products(auth, access_token, cursor="", query=""):
+    """
+    @description: 获取已售出的商品 Get sold products
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param cursor:
+    @param query: 查询关键词，可为空 Query keywords, can be empty
+    @return:
+    """
+    url = f"{base_url}/api/stockx/get_sold_products"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "cursor": cursor,
+        "query": query,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def get_history_products(auth, access_token, cursor="", query=""):
+    """
+    @description: 获取历史已售商品 Get historical sold products
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param cursor:
+    @param query: 查询关键词，可为空 Query keywords, can be empty
+    @return:
+    """
+    url = f"{base_url}/api/stockx/get_history_products"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "cursor": cursor,
+        "query": query,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def get_order_detail(auth, access_token, chain_id):
+    """
+    @description: 获取订单详情 Get order details
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param chain_id: chain_id
+    @return:
+    """
+    url = f"{base_url}/api/stockx/get_order_detail"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "chain_id": chain_id,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def vacation_mode(auth, access_token, customer_id, switch_to):
+    """
+    @description: 设置假期模式 Set vacation mode
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param customer_id: customer_id
+    @param switch_to: OFF:关闭假期模式 ON:开启假期模式 OFF: Turn off vacation mode ON: Turn on vacation mode
+    @return:
+    """
+    url = f"{base_url}/api/stockx/vacation_mode"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "customer_id": customer_id,
+        "switch_to": switch_to,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def cancel_product(auth, access_token, chain_id):
+    """
+    @description: 下架商品 Cancel product
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param chain_id: chain_id
+    @return:
+    """
+    url = f"{base_url}/api/stockx/cancel_product"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+        "chain_id": chain_id,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+# sold_product_list={"products": [{"chain_id": "", "sku": ""}]}
+def create_shipment(auth, access_token, sold_product_list):
+    """
+    @description: 创建发货单 Create shipment
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param sold_product_list: 已售出的商品列表 Sold product list
+    @return:
+    """
+    url = f"{base_url}/api/stockx/create_shipment"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+    }
+    response = requests.post(url, params=params, json=sold_product_list)
+    return response.json()
+
+
+# chain_id_price_dict={
+#     "chain_id": "",
+#     "price": "",
+#     "user_id": "",
+#     "expires_second": 3600,
+# }
+def update_price_single(auth, access_token, chain_id_price_dict):
+    """
+    @description: 单个修改价格 Single price modification
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param chain_id_price_dict:
+    @return:
+    """
+    url = f"{base_url}/api/stockx/create_shipment"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+    }
+    response = requests.post(url, params=params, json=chain_id_price_dict)
+    return response.json()
+
+
+# chain_id_price_dict = {
+#    "products": [{
+#       "chain_id": "",
+#       "price": "",
+#       "user_id": "",
+#       "expires_second": 3600,
+#    }]
+# }
+def update_price_multi(auth, access_token, chain_id_price_dict):
+    """
+    @description: 批量修改价格 Batch modification of price
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param chain_id_price_dict:
+    @return:
+    """
+    url = f"{base_url}/api/stockx/create_shipment"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+    }
+    response = requests.post(url, params=params, json=chain_id_price_dict)
+    return response.json()
+
+
+# product_info = {
+#    "product_uuid": "",
+#    "price": "",
+#    "expires_second": 3600,
+# }
+def listing_product_single(auth, access_token, product_info):
+    """
+    @description: 单个上架商品 Single listing product
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param product_info:
+    @return:
+    """
+    url = f"{base_url}/api/stockx/listing_product_single"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+    }
+    response = requests.post(url, params=params, json=product_info)
+    return response.json()
+
+
+# product_info = {
+#    "products": [{
+#       "product_uuid": "",
+#       "price": "",
+#       "expires_second": 3600,
+#    }]
+# }
+def listing_product_multi(auth, access_token, product_info):
+    """
+    @description: 批量上架商品 Batch listing product
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param product_info_list:
+    @return:
+    """
+    url = f"{base_url}/api/stockx/listing_product_multi"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "access_token": access_token,
+    }
+    response = requests.post(url, params=params, json=product_info)
+    return response.json()
+
+
+def get_product_size_info_by_sku(auth, sku):
+    """
+    @description: 获取商品尺码信息(内部缓存数据) Get product size information（internal cache data）
+    @param auth: 初始化auth接口返回的auth参数 auth parameter returned by the init_auth interface
+    @param access_token: 用户登录后的access_token参数 access_token parameter after user login
+    @param sku: sku
+    @return:
+    """
+    url = f"{base_url}/api/stockx/get_product_size_info_by_sku"
+    params = {
+        "token": TOKEN,
+        "auth": auth,
+        "sku": sku,
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+if __name__ == '__main__':
+    print(ping())
+    print(init_auth())
